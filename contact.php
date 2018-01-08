82121dd167167be5924ae27b6aad229911d051f7
hamza202 <k8668975@gmail.com>
2018-01-08 11:40:29 +0200

    three update

--- contact.php
+++ contact.php
@@ -38,14 +38,14 @@
 				<div class="row">
 					<div class="col-lg-12">
 						<div class="breadcrumbs-title text-center">
-							<h1>Contact</h1>
+							<h1>تواصل معنا</h1>
 							<div class="top-page">
 								<ul>
-									<li><a href="index.html">Home</a></li>
+									<li><a href="index.html">الرئيسية</a></li>
 									<li>
 										<span>&gt;</span>
 									</li>
-									<li>Contact</li>
+									<li>تواصل معنا</li>
 								</ul>
 							</div>
 						</div>
@@ -60,15 +60,15 @@
 				<div class="row">
 					<div class="col-sm-8">
 						<div class="contact-map">
-							<div id="googleMap"></div>
-						</div>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d324408.8003665608!2d50.73534257263062!3d26.058257221489743!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e48524e6a47a211%3A0x2e9450e2dbda1046!2z2KfZhNio2K3YsdmK2YY!5e0!3m2!1sar!2s!4v1515402670996" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>						</div>
 						<div class="address-info">
 							<div class="address-title text-center">
-								<h2>Our Address</h2>
+								<h2>عنواننا</h2>
 							</div>
 							<div class="col-md-6">
 								<div class="contact-info-p">
-									<p>Democratizing the global financial system; vaccines transform reproductive rights Bono protect. Meaningful turmoil cooperation reduce carbon emissions marginalized communities fundraise.</p>
+                                    <p>هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.
+                                        إذا كنت تحتاج إلى عدد أكبر من الفقرات يتيح لك مولد النص العربى زيادة </p>
 								</div>
 							</div>
 							<div class="col-md-6">
@@ -76,22 +76,22 @@
 									<ul>
 										<li>
 											<i class="fa fa-map-marker"></i>
-											<strong>Address:</strong>
-											 4321 Your Address, Country
+											<strong>العنوان :</strong>
+											 البحرين المنامة
 										</li>
 										<li>
 											<i class="fa fa-envelope"></i>
-											<strong>Email:</strong>
-											 info@abcds.com
+											<strong>بريد الكتروني :</strong>
+											 info@info.com
 										</li>
 										<li>
 											<i class="fa fa-phone-square"></i>
-											<strong>Phone:</strong>
-											+12 345 678 9123
+											<strong>رقم هاتف :</strong>
+											0123456789
 										</li>
 										<li>
 											<i class="fa fa-dribbble"></i>
-											<strong>Website:</strong> 
+											<strong>موقعنا :</strong>
 											www.abdfhrg.com
 										</li>
 									</ul>
@@ -101,30 +101,30 @@
 					</div>
 					<div class="col-sm-4">
 						<div class="address-info right-address-info">
-							<form action="https://tvlgiao.github.io/html-general-demo/general/mail.php" method="POST">
-								<h3>Get in touch</h3>
+							<form action="#" method="POST">
+								<h3>كن على تواصل معنا</h3>
 								<p class="form-row-wide">
-									Name (required)
+									<sup class="required">*</sup>الأسم
 									<br />
-									<input type="text" name="name" id="name">
+									<input type="text" name="name" id="name" required>
 								</p>
 								<p class="form-row-wide">
-									Email (required)
+                                    <sup class="required">*</sup>بريد الكتروني
 									<br />
-									<input type="email" name="email" id="email" />
+									<input type="email" name="email" id="email" required/>
 								</p>
 								<p class="form-row-wide">
-									Subject
+									العنوان
 									<br />
 									<input type="text" name="subject" id="subject" />
 								</p>
 								<p class="form-row-wide">
-									Message
+                                    <sup class="required">*</sup>الرسالة
 									<br />
-									<textarea name="message" id="message"></textarea>
+									<textarea name="message" id="message" required></textarea>
 								</p>
 								<p class="form-row pull-right">
-									<input type="submit" id="submit" name="submit" class="button" value="send">
+									<input type="submit" id="submit" name="submit" class="button" value="ارسال">
 								</p>
 							</form>
 						</div>
@@ -133,252 +133,8 @@
 			</div>
 		</div>
 		<!-- Contact Us Area End -->
-		<!-- Latest News Area start -->
-		<div class="blog-area section-padding">
-			<div class="container-fluid">
-				<div class="row">
-					<div class="col-lg-12">
-						<div class="section-title">
-							<h2><span>LATEST NEWS</span></h2>
-						</div>						
-					</div>
-					<div class="col-sm-4">
-						<div class="single-blog">
-							<div class="post-info-thumbnail">
-								<div class="thumbnail-content">
-									<a href="#" class="thumbnail effect-color effect-color-1">
-										<img src="img/blog/1.jpg" alt="" />
-									</a>
-								</div>
-								<div class="blog-post-date">
-									<p class="day">24</p>
-									<p class="month">May</p>
-								</div>
-							</div>
-							<div class="post-info-content">
-								<h3 class="heading-title">
-									<a title="Maecenas vitae urna" href="#">Maecenas vitae urna</a>
-								</h3>
-								<div class="post-info-author">
-									<span class="author">
-										<i class="fa fa-user"></i>
-										<a title="Posts by admin" href="#">Admin</a>
-									</span>
-									<span class="comments-count">
-										<i class="fa fa-comment-o"></i>
-										2 Comments
-									</span>
-								</div>
-								<p class="post-details">Midi indigo vintage boots tortoise-shell sunglasses bomber leather A.P.C. Clutch powder blue oversized sweatshirt slip dress skort sports luxe gold collar collarless skirt</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-sm-4">
-						<div class="single-blog">
-							<div class="post-info-thumbnail">
-								<div class="thumbnail-content">
-									<a href="#" class="thumbnail effect-color effect-color-1">
-										<img alt="" src="img/blog/2.jpg">
-									</a>
-								</div>
-								<div class="blog-post-date">
-									<p class="day">29</p>
-									<p class="month">May</p>
-								</div>
-							</div>
-							<div class="post-info-content">
-								<h3 class="heading-title">
-									<a title="Democratizing" href="#">Democratizing</a>
-								</h3>
-								<div class="post-info-author">
-									<span class="author">
-										<i class="fa fa-user"></i>
-										<a title="Posts by admin" href="#">Admin</a>
-									</span>
-									<span class="comments-count">
-										<i class="fa fa-comment-o"></i>
-										1 Comments
-									</span>
-								</div>
-								<p class="post-details">Midi indigo vintage boots tortoise-shell sunglasses bomber leather A.P.C. Clutch powder blue oversized sweatshirt slip dress skort sports luxe gold collar collarless skirt</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-sm-4">
-						<div class="single-blog">
-							<div class="post-info-thumbnail">
-								<div class="thumbnail-content">
-									<a href="#" class="thumbnail effect-color effect-color-1">
-										<img alt="" src="img/blog/3.jpg">
-									</a>
-								</div>
-								<div class="blog-post-date">
-									<p class="day">05</p>
-									<p class="month">Jun</p>
-								</div>
-							</div>
-							<div class="post-info-content">
-								<h3 class="heading-title">
-									<a title="Braid slipper" href="#">Braid slipper</a>
-								</h3>
-								<div class="post-info-author">
-									<span class="author">
-										<i class="fa fa-user"></i>
-										<a title="Posts by admin" href="#">Admin</a>
-									</span>
-									<span class="comments-count">
-										<i class="fa fa-comment-o"></i>
-										3 Comments
-									</span>
-								</div>
-								<p class="post-details">Midi indigo vintage boots tortoise-shell sunglasses bomber leather A.P.C. Clutch powder blue oversized sweatshirt slip dress skort sports luxe gold collar collarless skirt</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>		
-		<!-- Latest News Area End -->			
 		<!-- Footer Area Start -->
-		<footer class="footer-area">
-			<div class="footer-top-area section-padding">
-				<div class="container-fluid">
-					<div class="row">
-						<div class="col-md-3 col-sm-3 col-xs-12">
-							<div class="single-footer-widget">
-								<h3 class="footer-widget-title">CONTACT</h3>
-								<div class="text-widget">
-									<ul>
-										<li>
-											<i class="fa fa-map-marker"></i>
-											<span>249 Ung Van Khiem St., Binh ThanhDist, HCMC</span>
-										</li>
-										<li>
-											<i class="fa fa-phone"></i>
-											<span>+084 0123 456 999</span>
-										</li>
-										<li>
-											<i class="fa fa-envelope"></i>
-											<span>Support@Bootexpert.com</span>
-										</li>
-									</ul>
-								</div>
-							</div>
-						</div>
-						<div class="col-md-2 hidden-sm col-xs-12">
-							<div class="single-footer-widget">
-								<h3 class="footer-widget-title">About Super Store</h3>
-								<div class="text-widget">
-									<ul>
-										<li><a href="#">Meet the team</a></li>
-										<li><a href="#">Blogs</a></li>
-										<li><a href="#">Our Partner</a></li>
-										<li><a href="#">Job & Careerson</a></li>
-									</ul>
-								</div>
-							</div>
-						</div>
-						<div class="col-md-2 col-sm-3 col-xs-12">
-							<div class="single-footer-widget">
-								<h3 class="footer-widget-title">Why Buy From Us</h3>
-								<div class="text-widget">
-									<ul>
-										<li><a href="#">Shipping & returns</a></li>
-										<li><a href="#">Secure Shopping</a></li>
-										<li><a href="#">Free Shopping Program</a></li>
-										<li><a href="#">Affiliates</a></li>
-									</ul>
-								</div>
-							</div>
-						</div>
-						<div class="col-md-2 col-sm-3 col-xs-12">
-							<div class="single-footer-widget">
-								<h3 class="footer-widget-title">Follow</h3>
-								<div class="social-icons">
-									<ul>
-										<li class="icon-facebook">
-											<a href="#" title="Become our fan">
-												<i class="fa fa-facebook"></i>
-											</a>
-										</li>
-										<li class="icon-twitter">
-											<a href="#" title="Follow us">
-												<i class="fa fa-twitter"></i>
-											</a>
-										</li>
-										<li class="icon-google">
-											<a href="#" title="Get updates">
-												<i class="fa fa-google-plus"></i>
-											</a>
-										</li>
-										<li class="icon-pin">
-											<a href="#" title="See Us">
-												<i class="fa fa-pinterest"></i>
-											</a>
-										</li>
-										<li class="icon-linkedin">
-											<a href="#" title="See Us">
-												<i class="fa fa-linkedin"></i>
-											</a>
-										</li>
-									</ul>
-								</div>
-							</div>
-						</div>
-						<div class="col-sm-3 col-xs-12">
-							<div class="single-footer-widget">
-								<h3 class="footer-widget-title">Newsletter Sign up</h3>
-								<div class="news-letter">
-									<label>We will only send good news</label>
-									<input type="text" placeholder="enter your email address"/>
-									<button type="submit">
-										<i class="fa fa-paper-plane" aria-hidden="true"></i>
-									</button>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="footer-bottom-area">
-				<div class="container-fluid">
-					<div class="col-sm-6 col-xs-12">
-						<p>Copyright <a target="_blank" href="http://bootexperts.com/">BootExperts</a> All rights reserved.</p>
-					</div>
-					<div class="col-sm-6 col-xs-12">
-						<div class="payment">
-							<ul>
-								<li>
-									<a href="#">
-										<img src="img/payment/1.jpg" alt="" />
-									</a>
-								</li>
-								<li>
-									<a href="#">
-										<img src="img/payment/2.jpg" alt="" />
-									</a>
-								</li>
-								<li>
-									<a href="#">
-										<img src="img/payment/3.jpg" alt="" />
-									</a>
-								</li>
-								<li>
-									<a href="#">
-										<img src="img/payment/4.jpg" alt="" />
-									</a>
-								</li>
-								<li>
-									<a href="#">
-										<img src="img/payment/5.jpg" alt="" />
-									</a>
-								</li>
-							</ul>
-						</div>						
-					</div>
-				</div>
-			</div>	
-		</footer>
+		<?php include ('footer.php')?>
 		<!-- Footer Area End -->	
 		<!-- all js here -->
 		<!-- jquery latest version -->
@@ -405,24 +161,6 @@
         <script src="js/wow.min.js"></script>
 		<!-- Google Map APi
 		============================================ -->
-		<script src="https://maps.googleapis.com/maps/api/js"></script>
-		<script>
-			function initialize() {
-			  var mapOptions = {
-				zoom: 14,
-				scrollwheel: false,
-				center: new google.maps.LatLng(37.7928294, -122.4000693)
-			  };
-			  var map = new google.maps.Map(document.getElementById('googleMap'),
-				  mapOptions);
-			  var marker = new google.maps.Marker({
-				position: map.getCenter(),
-				animation: google.maps.Animation.BOUNCE,
-				map: map
-			  });
-			}
-			google.maps.event.addDomListener(window, 'load', initialize);				
-		</script>		
 		<script>
 			new WOW().init();
 		</script>
@@ -434,5 +172,4 @@
         <script src="js/main.js"></script>
     </body>
 
-<!-- Mirrored from tvlgiao.github.io/html-general-demo/general/contact.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 06 Jan 2018 08:24:06 GMT -->
 </html>
---
+++ footer.php
@@ -0,0 +1,75 @@
+<footer class="footer-area footer-area-eight">
+    <div class="footer-top-area section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <div class="single-footer-widget">
+                        <h3 class="footer-widget-title">تواصل معنا</h3>
+                        <div class="text-widget">
+                            <ul>
+                                <li>
+                                    <i class="fa fa-map-marker"></i>
+                                    <span>البحرين - المنامة</span>
+                                </li>
+                                <li>
+                                    <i class="fa fa-phone"></i>
+                                    <span>123456789</span>
+                                </li>
+                                <li>
+                                    <i class="fa fa-envelope"></i>
+                                    <span>info@info.com</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="single-footer-widget">
+                        <h3 class="footer-widget-title">الوصول السريع</h3>
+                        <div class="text-widget color-hover">
+                            <ul>
+                                <li><a href="#">الفريق</a></li>
+                                <li><a href="#">المدونة</a></li>
+                                <li><a href="#">شركائنا</a></li>
+                                <li><a href="#">الشروط</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="single-footer-widget">
+                        <h3 class="footer-widget-title">روابط اخرى</h3>
+                        <div class="text-widget color-hover">
+                            <ul>
+                                <li><a href="#">خصومات مميزة</a></li>
+                                <li><a href="#">المنتجات</a></li>
+                                <li><a href="#">الشركات</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="single-footer-widget">
+                        <h3 class="footer-widget-title">مساعدة</h3>
+                        <div class="text-widget">
+                            <ul>
+                                <li><a href="#">اسألة شائعة</a></li>
+                                <li><a href="#">نبذة عنا</a></li>
+                                <li><a href="#">تواصل معنا</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom-area">
+        <div class="container">
+            <div class="col-sm-6">
+                <p> حقوق النشر  <a target="_blank" href="#">Promos 2018</a>    جميع الحقوق محفوظة
+                </p>
+            </div>
+
+        </div>
+    </div>
+</footer>
