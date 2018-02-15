2d1539b1b4de60437b2215977d511f5e0511ff90
hamza202 <k8668975@gmail.com>
2018-02-15 14:03:18 +0200

    first

---
+++ blog.php
@@ -0,0 +1,309 @@
+<!doctype html>
+<html class="no-js">
+
+<!-- Mirrored from tvlgiao.github.io/html-general-demo/general/login.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 06 Jan 2018 08:24:18 GMT -->
+<!-- Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=utf-8"/><!-- /Added by HTTrack -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Login or Register || General Online Shop</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- google fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Poppins:400,300,500,600,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Archivo+Narrow:400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Satisfy' rel='stylesheet' type='text/css'>
+
+    <!-- all css here -->
+    <!-- style css -->
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/fileinput.css">
+
+    <link rel="stylesheet" href="css/space.css">
+
+    <!-- modernizr JS -->
+    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+<body>
+<!--[if lt IE 8]>
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade
+    your browser</a> to improve your experience.</p>
+<![endif]-->
+
+<?php include('header.php') ?>
+
+<!-- Header Area End -->
+<!-- Breadcrumbs Area Start -->
+<div class="breadcrumbs-area section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="breadcrumbs-title text-center">
+                    <h1>مقالات </h1>
+                    <div class="top-page">
+                        <ul>
+                            <li><a href="index.php">الرئيسية </a></li>
+                            <li>> مقالات</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Breadcrumbs Area End -->
+<!-- Login Area Start -->
+<!-- Bolg Area Start -->
+<div class="blog-page-content section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-9">
+                <div class="blog-content-list">
+                    <div class="single-blog-content">
+                        <div class="post-info-thumbnail">
+                            <div class="thumbnail-content">
+                                <a href="#" class="thumbnail effect-color effect-color-1">
+                                    <img src="img/blog/bg/1.jpg" alt=""/>
+                                </a>
+                            </div>
+                            <div class="blog-post-date">
+                                <p class="day">24</p>
+                                <p class="month">May</p>
+                                <p class="year">2016</p>
+                            </div>
+                        </div>
+                        <div class="post-info-content">
+                            <h3 class="heading-title">
+                                <a title="Maecenas vitae urna" href="#">نص تجريبي</a>
+                            </h3>
+                            <div class="post-info-author">
+										<span class="author color-hover">
+											<i class="fa fa-user"></i>
+											<a title="Posts by admin" href="#">حمزة</a>
+										</span>
+                                <span class="entry-date">
+											<i class="fa fa-clock-o"></i>
+											October 13, 2015
+										</span>
+                            </div>
+                            <p class="post-details-info">هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم
+                                توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص
+                                الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.
+                                إذا كنت تحتاج إلى عدد أكبر من الفقرات يتيح لك مولد النص العربى زيادة عدد الفقرات كما
+                                تريد، النص لن يبدو مقسما ولا يحوي أخطاء لغوية، مولد النص العربى مفيد لمصممي المواقع على
+                                وجه الخصوص، حيث يحتاج العميل فى كثير من الأحيان أن يطلع على صورة حقيقية لتصميم
+                                الموقع.</p>
+                            <a href="single-blog.html" class="read-more">أقرأ المزيد</a>
+                        </div>
+                    </div>
+                    <div class="single-blog-content">
+                        <div class="post-info-thumbnail">
+                            <div class="thumbnail-content">
+                                <a href="#" class="thumbnail effect-color effect-color-1">
+                                    <img src="img/blog/bg/2.jpg" alt=""/>
+                                </a>
+                            </div>
+                            <div class="blog-post-date">
+                                <p class="day">24</p>
+                                <p class="month">May</p>
+                                <p class="year">2016</p>
+                            </div>
+                        </div>
+                        <div class="post-info-content">
+                            <h3 class="heading-title">
+                                <a title="Maecenas vitae urna" href="#">نص تجريبي</a>
+                            </h3>
+                            <div class="post-info-author">
+										<span class="author color-hover">
+											<i class="fa fa-user"></i>
+											<a title="Posts by admin" href="#">حمزة</a>
+										</span>
+                                <span class="entry-date">
+											<i class="fa fa-clock-o"></i>
+											October 13, 2015
+										</span>
+                            </div>
+                            <p class="post-details-info">هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم
+                                توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص
+                                الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.
+                                إذا كنت تحتاج إلى عدد أكبر من الفقرات يتيح لك مولد النص العربى زيادة عدد الفقرات كما
+                                تريد، النص لن يبدو مقسما ولا يحوي أخطاء لغوية، مولد النص العربى مفيد لمصممي المواقع على
+                                وجه الخصوص، حيث يحتاج العميل فى كثير من الأحيان أن يطلع على صورة حقيقية لتصميم
+                                الموقع.</p>
+                            إذا كنت تحتاج إلى عدد أكبر من الفقرات يتيح لك مولد النص العربى زيادة عدد الفقرات كما تريد،
+                            النص لن يبدو مقسما ولا يحوي أخطاء لغوية، مولد النص العربى مفيد لمصممي المواقع على وجه
+                            الخصوص، حيث يحتاج العميل فى كثير من الأحيان أن يطلع على صورة حقيقية لتصميم الموقع.</p>
+                            <a href="single-blog.html" class="read-more">أقرأ المزيد</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="sidebar-widget">
+                    <aside class="recent-post-widget widget">
+                        <h3 class="widget-title">اخر المنشورات</h3>
+                        <ul class="recentposts">
+                            <li class="media">
+                                <div class="entry-title">
+                                    <a href="#">نص تجريبي </a>
+                                    <p class="entry-desc"> إذا كنت تحتاج إلى عدد أكبر من الفقرات يتيح لك مولد النص
+                                        العربى زيادة عدد الفقرات كما تريد، النص لن يبدو مقسما ولا يحوي أخطاء لغوية، مولد
+                                        النص العربى مفيد لمصممي المواقع على وجه الخصوص، حيث يحتاج العميل فى كثير من
+                                        الأحيان أن يطلع على صورة حقيقية لتصميم الموقع.</p>
+                                    </p>
+                                </div>
+                                <p class="entry-meta">
+                                    <i class="fa fa-clock-o"></i>
+                                    October 13, 2015
+                                </p>
+                                <p class="author">
+                                    <i class="fa fa-user"></i>
+                                    <a title="Posts by admin" href="#">admin</a>
+                                </p>
+                            </li>
+                            <li class="media">
+                                <div class="entry-title">
+                                    <a href="#">نص تجريبي </a>
+                                    <p class="entry-desc"> إذا كنت تحتاج إلى عدد أكبر من الفقرات يتيح لك مولد النص
+                                        العربى زيادة عدد الفقرات كما تريد، النص لن يبدو مقسما ولا يحوي أخطاء لغوية، مولد
+                                        النص العربى مفيد لمصممي المواقع على وجه الخصوص، حيث يحتاج العميل فى كثير من
+                                        الأحيان أن يطلع على صورة حقيقية لتصميم الموقع.</p>
+                                </div>
+                                <p class="entry-meta">
+                                    <i class="fa fa-clock-o"></i>
+                                    October 13, 2015
+                                </p>
+                                <p class="author">
+                                    <i class="fa fa-user"></i>
+                                    <a title="Posts by admin" href="#">admin</a>
+                                </p>
+                            </li>
+                            <li class="media">
+                                <div class="entry-title">
+                                    <a href="#">نص تجريبي </a>
+                                    <p class="entry-desc"> إذا كنت تحتاج إلى عدد أكبر من الفقرات يتيح لك مولد النص
+                                        العربى زيادة عدد الفقرات كما تريد، النص لن يبدو مقسما ولا يحوي أخطاء لغوية، مولد
+                                        النص العربى مفيد لمصممي المواقع على وجه الخصوص، حيث يحتاج العميل فى كثير من
+                                        الأحيان أن يطلع على صورة حقيقية لتصميم الموقع.</p>
+                                </div>
+                                <p class="entry-meta">
+                                    <i class="fa fa-clock-o"></i>
+                                    October 13, 2015
+                                </p>
+                                <p class="author">
+                                    <i class="fa fa-user"></i>
+                                    <a title="Posts by admin" href="#">admin</a>
+                                </p>
+                            </li>
+                        </ul>
+                    </aside>
+                    <aside class="banner-widget widget">
+                        <div class="single-effect">
+                            <a href="#">
+                                <div class="bg-effect">
+                                    <img src="img/banner/s-1.jpg" alt="">
+                                </div>
+                                <span class="s s1"></span>
+                                <span class="s s2"></span>
+                                <span class="s s3"></span>
+                                <span class="s s4"></span>
+                            </a>
+                        </div>
+                    </aside>
+                    <aside class="banner-widget widget">
+                        <div class="single-effect">
+                            <a href="#">
+                                <div class="bg-effect">
+                                    <img src="img/banner/s-2.jpg" alt="">
+                                </div>
+                                <span class="s s1"></span>
+                                <span class="s s2"></span>
+                                <span class="s s3"></span>
+                                <span class="s s4"></span>
+                            </a>
+                        </div>
+                    </aside>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Bolg Area End -->
+<!-- Login Area End -->
+
+<?php include('footer.php') ?>
+<!-- all js here -->
+<!-- jquery latest version -->
+<script src="js/vendor/jquery-1.12.0.min.js"></script>
+<!-- bootstrap js -->
+<script src="js/bootstrap.min.js"></script>
+<!-- bootstrap js -->
+<script src="js/fileinput.min.js"></script>
+<script>
+    $(document).on('ready', function () {
+        $("#b2b").fileinput();
+    });
+</script>
+<script src="js/bootstrap-select.min.js"></script>
+<!-- owl.carousel js -->
+<script src="js/owl.carousel.min.js"></script>
+<!-- jquery-ui js -->
+<script src="js/jquery-ui.min.js"></script>
+<!-- Img Zoom js -->
+<script src="js/jquery.simpleLens.min.js"></script>
+<!-- fancybox js -->
+<script src="js/jquery.fancybox.pack.js"></script>
+<!-- jquery countdown js -->
+<script src="js/jquery.countdown.min.js"></script>
+<!-- Nivo Slider JS -->
+<script type="text/javascript" src="js/jquery.nivo.slider.pack.js"></script>
+<!-- meanmenu js -->
+<script src="js/jquery.meanmenu.js"></script>
+<!-- wow js -->
+<script src="js/wow.min.js"></script>
+<script>
+
+    $('input[name="optionsRadios"]').on('change', function () {
+        if ($(this).val() == 'update') {
+
+            //change to "show update"
+            $("#cont").text("اسم المستخدم");
+            $("#reg-img-label").text("اضافة صورة شخصية");
+
+        } else {
+
+            $("#cont").text("اسم الشركة");
+            $("#reg-img-label").text("اضافة شعار الشركة");
+        }
+    });
+
+    new WOW().init();
+
+    var password = document.getElementById("password")
+        , confirm_password = document.getElementById("confirm_password");
+
+    function validatePassword() {
+        if (password.value != confirm_password.value) {
+            confirm_password.setCustomValidity("كلمة المرور لا تطابق");
+        } else {
+            confirm_password.setCustomValidity('');
+        }
+    }
+
+    password.onchange = validatePassword;
+    confirm_password.onkeyup = validatePassword;
+</script>
+<!-- scrollUp JS -->
+<script src="js/jquery.scrollUp.min.js"></script>
+<!-- plugins js -->
+<script src="js/plugins.js"></script>
+<!-- main js -->
+<script src="js/main.js"></script>
+</body>
+
+</html>
